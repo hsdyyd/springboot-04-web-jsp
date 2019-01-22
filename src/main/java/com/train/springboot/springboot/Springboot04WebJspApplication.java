e85c5444a51f2a91e5cedbbb3f379e49ddfba110
hsdyyd <yidong06@sina.com>
2019-01-22 12:00:55 +0800

    工程完成

---
+++ src/main/java/com/train/springboot/springboot/Springboot04WebJspApplication.java
@@ -0,0 +1,14 @@
+package com.train.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04WebJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot04WebJspApplication.class, args);
+    }
+
+}
+
